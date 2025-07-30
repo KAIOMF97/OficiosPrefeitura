21d0bb9a24c35077cdbe00b5f28e5a2a0bdd7eec
Kaio Mangolin Fernandes <kaiomangolinfernandes@alunos.utfpr.edu.br>
2025-07-30 17:10:25 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -8,8 +8,16 @@ $data = $_POST["data"];
 $assunto = $_POST["assunto"];
 
 $linhas = file("oficios.csv", FILE_IGNORE_NEW_LINES);
-$numero = count($linhas) + 1;
+$maiorNumero = 264; // Começa em 264, o próximo será 265
 
-$linha = "$numero,$data,$assunto" . PHP_EOL;
-file_put_contents("oficios.csv", $linha, FILE_APPEND);
+foreach ($linhas as $linha) {
+  $partes = explode(",", $linha);
+  if (is_numeric($partes[0]) && (int)$partes[0] > $maiorNumero) {
+    $maiorNumero = (int)$partes[0];
+  }
+}
+
+$novoNumero = $maiorNumero + 1;
+$novaLinha = "$novoNumero,$data,$assunto" . PHP_EOL;
+file_put_contents("oficios.csv", $novaLinha, FILE_APPEND);
 ?>
